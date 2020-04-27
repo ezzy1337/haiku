411da5b10746f76cb2696bfb44e79db3d1471183
Derek Drummond <ddrummond@cylance.com>
2020-04-27 11:18:26 -0500

    Added Haiku generator

---
+++ main.go
@@ -0,0 +1,11 @@
+package haiku
+
+import (
+  "fmt"
+)
+
+func generate() string {
+  return fmt.Sprintf("a haiku promised %c although random it is not %c so sorry I lied %c", '\n', '\n', '\n')
+}
+
+
