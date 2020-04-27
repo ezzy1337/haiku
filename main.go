ded0d94cdab4a08feca3c8e739d8c80484543165
Derek Drummond <ddrummond@cylance.com>
2020-04-27 11:44:22 -0500

    Export generate function

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package haiku
 
 import "fmt"
 
-func generate() string {
+func Generate() string {
   return fmt.Sprintf("a haiku promised %c although random it is not %c so sorry I lied %c", '\n', '\n', '\n')
 }
 
